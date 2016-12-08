0040b1291f2ddfc383523677c0868f2e5a8bef77
Evan Rowe <evan.rowe@healthsparq.com>
2016-12-08 14:04:38 -0800

    goodbye feature flag file

--- vendor/feature-flags.js
+++
@@ -1,9 +0,0 @@
-
-        var featureFlags = {"DEVELOPMENT":true,"PRODUCTION":false,"TEST":false,"RADICAL":true};
-        if(window) {
-          for (var feature in featureFlags) {
-            if (featureFlags.hasOwnProperty(feature)) {
-              window[feature] = featureFlags[feature];
-            }
-          }
-        }
