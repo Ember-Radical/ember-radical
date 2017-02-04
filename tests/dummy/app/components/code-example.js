4385145b46485f30cba33bc82f1328eec0f142f9
Evan Rowe <ev.rowe@gmail.com>
2017-02-04 11:05:47 -0800

    ✨ Adds code-example component for easy display of code and rendered example, wrapped within a rad-card

---
+++ tests/dummy/app/components/code-example.js
@@ -0,0 +1,42 @@
+import Ember from 'ember';
+import hbs from 'htmlbars-inline-precompile';
+const { Component } = Ember;
+
+export default Component.extend({
+
+  code: '',
+
+  language: '',
+
+  showCode: false,
+
+  title: '',
+
+  actions: {
+    toggleCode() {
+      this.toggleProperty('showCode');
+    }
+  },
+
+  layout: hbs`
+    {{#rad-card as |components|}}
+      {{#components.title
+        classNames='code-card-title'}}
+        <span class="example-title">{{title}}</span>
+        {{#rad-button
+          brand='secondary'
+          classNames='code-toggle-button'
+          click=(action 'toggleCode')}}<b>&lt;/&gt;</b>{{/rad-button}}
+      {{/components.title}}
+      {{#components.body}}
+        <div class="code-sample {{if showCode '' 'hidden'}}">
+          {{highlight-code code=code language=language}}
+        </div>
+        <div class="rendered-sample">
+          {{!-- {{code}} --}}
+          {{yield}}
+        </div>
+      {{/components.body}}
+    {{/rad-card}}
+  `
+});
