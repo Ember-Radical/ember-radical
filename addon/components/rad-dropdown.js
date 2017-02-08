fb7cf3fb89fd8c4729487f03be3ae117a101659e
Dan Hedgecock <Hedgecock.D@gmail.com>
2017-02-08 14:53:27 -0800

    Feature/dropdowns upgrade (#59)
    Another rad upgrade for our rad drodpowns :sparkles:

--- addon/components/rad-dropdown.js
+++ addon/components/rad-dropdown.js
@@ -1,6 +1,7 @@
 import Component from 'ember-component';
-import computed from 'ember-computed';
 import hbs from 'htmlbars-inline-precompile';
+import run from 'ember-runloop';
+import $ from 'jquery';
 
 import { bindOnEscape, unbindOnEscape } from '../utils/listeners';
 
@@ -12,13 +13,13 @@ import { bindOnEscape, unbindOnEscape } from '../utils/listeners';
  *
  * ## Usage
  *
- * ### 1. Simple invocation. It's so easy. Just pass a Target and Content prop
+ * #### 1. Simple invocation. It's so easy. Just pass a Target and Content prop
  * and you've got a totally rad dropdown.
  * ```glimmer
  * {{rad-dropdown Target="Open me!" Content="Hey, what's up?"}}
  * ```
  *
- * ### 2. Simple target with custom content. Use the Content contextual
+ * #### 2. Simple target with custom content. Use the Content contextual
  * component to add custom content to the dropdown.
  * ```handlebars
  * {{#rad-dropdown Target="Open me!" as |components|}}
@@ -26,7 +27,7 @@ import { bindOnEscape, unbindOnEscape } from '../utils/listeners';
  * {{/rad-dropdown}}
  * ```
  *
- * ### 3. Customize both the dropdown target and content.
+ * #### 3. Customize both the dropdown target and content.
  * ```glimmer
  * {{#rad-dropdown as |components|}}
  *   {{#components.target}}Open me!{{/components.target}}
@@ -34,13 +35,13 @@ import { bindOnEscape, unbindOnEscape } from '../utils/listeners';
  * {{/rad-dropdown}}
  * ```
  *
- * ### 4. Turn the target into a button with `buttonStyle=true` and pass a
+ * #### 4. Turn the target into a button with `buttonStyle=true` and pass a
  * `brand` to change the color button.
  * ```handlebars
  * {{rad-dropdown buttonStyle=true brand="primary" Target="Open me!" Content="Hey, what's up?"}}
  * ```
  *
- * ### 5. Use as a dropdown menu with menu items
+ * #### 5. Use as a dropdown menu with menu items
  *
  * ```glimmer
  * {{#rad-dropdown dropdownMenu=true brand="primary" buttonStyle=true as |components|}}
@@ -54,10 +55,16 @@ import { bindOnEscape, unbindOnEscape } from '../utils/listeners';
  * {{/rad-dropdown}}
  * ```
  *
+ * #### 6. Pass `autoClose` for a self closing dropdown
+ * ```glimmer
+ * {{rad-dropdown autoClose=true Target="Open" Content="Mouse out and I'll close myself!"}}
+ * ```
+ *
  * Configuration | Type | Default | Description
  * --- | --- | --- | ---
  * `buttonStyle` | boolean | false | Whether to style the `target` to look like a button
  * `brand` | string | '' | The brand class to use to style the `target`
+ * `autoClose` | boolean| | false | Pass `true` for an auto closing dropdown on mouse out
  *
  * @class Component.RadDropdown
  * @constructor
@@ -66,6 +73,16 @@ import { bindOnEscape, unbindOnEscape } from '../utils/listeners';
 export default Component.extend({
   // Passed Properties
   // ---------------------------------------------------------------------------
+  /**
+   * When `autoClose` is set to true, the dropdown will automatically close
+   * itself 2.5sec after the user mouses out of it.
+   * @property autoClose
+   * @type {Boolean}
+   * @passed
+   * @optional
+   * @default false
+   */
+  autoClose: false,
   /**
    * Adds a brand class to the target as btn-{brand}
    * @property brand
@@ -92,17 +109,27 @@ export default Component.extend({
    * Fires when the dropdown is closed
    * @property onHide
    * @closure
+   * @passed
+   * @optional
    */
-  onHide: () => {},
   /**
    * Fires when the dropdown is opened
    * @property onShow
    * @closure
+   * @passed
+   * @optional
    */
-  onShow: () => {},
 
   // Properties
   // ---------------------------------------------------------------------------
+  /**
+   * Stored reference to `run.later` created in `mouseLeave` that will close any
+   * open dropdown. Is used in `mouseEnter` to cancel auto close.
+   * @property _autoCloseRunLater
+   * @type {?Function}
+   * @default null
+   */
+  _autoCloseRunLater: null,
   /**
    * State boolean for display of the dropdown content. Is toggled true/false to
    * handle show/hide. Updated in `_showContent` and `_hideContent`
@@ -128,8 +155,39 @@ export default Component.extend({
    */
   classNames: ['rad-dropdown'],
 
+  // Methods
+  // ---------------------------------------------------------------------------
+
+  /**
+   * When the dropdown is opened, bind an event listener that will close it if
+   * the user clicks anywhere other than the dropdown.
+   * @method _bindDropdownListeners
+   * @protected
+   */
+  _bindDropdownListeners() {
+    $('body').on(`mouseup.${this.get('elementId')}`, e => {
+      // Check if the click was inside the dropdown
+      let clickInDropdown = $(e.target).closest(`#${this.get('elementId')}`).length ? true : false;
+
+      // If the click was ouside dropdown, close the dropdown and then cleanup the listener
+      if (!clickInDropdown) {
+        this.send('hide');
+      }
+    });
+  },
+  /**
+   * Remove click listener from body. Used to DRY up our cleanup code in the
+   * supported click and mouse out close liseners.
+   * @method _unbindClickListener
+   * @protected
+   */
+  _unbindClickListener() {
+    $('body').off(`mouseup.${this.get('elementId')}`);
+  },
+
   // Hooks
   // ---------------------------------------------------------------------------
+
   /**
    * Safety first!
    * If we leave the page without closing the dropdown we don't want to orphan
@@ -141,7 +199,40 @@ export default Component.extend({
     if (this.get('onDestroy')) { this.get('onDestroy')(); }
 
     // Remove listeners
+    this._unbindClickListener();
     unbindOnEscape(this.get('elementId'));
+  },
+
+  // Events
+  // ---------------------------------------------------------------------------
+
+  /**
+   * Check for an auto-close timer when user mouses into dropdown and cancel it
+   * if it exists.
+   * @event mouseEnter
+   */
+  mouseEnter() {
+    let { autoClose, _autoCloseRunLater } = this.getProperties('autoClose', '_autoCloseRunLater');
+
+    if (autoClose && _autoCloseRunLater) {
+      run.cancel(_autoCloseRunLater);
+      this.set('_autoCloseRunLater', null);
+    }
+  },
+  /**
+   * When user mouses out of the dropdown start an auto close timer to close
+   * dropdown after 2.5 seconds. Is canceled using `mouseEnter` if user mouses
+   * back into dropdown.
+   * @event mouseLeave
+   */
+  mouseLeave() {
+    if (this.get('autoClose')) {
+      this.set('_autoCloseRunLater', run.later(() => {
+        if (!this.isDestroyed) {
+          this.send('hide');
+        }
+      }, 2500));
+    }
   },
 
   // Actions
@@ -159,14 +250,18 @@ export default Component.extend({
      * @return {undefined}
      */
     show() {
+      // Fire user hooks
+      if (this.get('onShow')) { this.get('onShow')(...arguments); }
+
       // Toggle display
       this.set('hidden', false);
 
-      // Check for passed closures
-      if (this.get('onShow')) { this.get('onShow')(...arguments); }
-
-      // Bind the keycommand `esc` to close dropdown
+      // Add click listeners and bind esc to close
+      this._bindDropdownListeners();
       bindOnEscape(this.get('elementId'), this.get('actions.hide').bind(this));
+
+      // Fire user hooks
+      if (this.get('onShown')) { this.get('onShown')(...arguments); }
     },
     /**
      * Handle the hiding of the dropdown. This will pass on any arguments you
@@ -175,28 +270,24 @@ export default Component.extend({
      * @return {undefined}
      */
     hide() {
+      // Fire user hooks
+      if (this.get('onHide')) { this.get('onHide')(...arguments); }
+
       // Toggle display
       this.set('hidden', true);
 
-      // Check for passed closures
-      if (this.get('onHide')) { this.get('onHide')(...arguments); }
-
       // Remove listeners
+      this._unbindClickListener();
       unbindOnEscape(this.get('elementId'));
+
+      // Fire user hooks
+      if (this.get('onHidden')) { this.get('onHidden')(...arguments); }
     }
   },
 
   // Layout
   // ---------------------------------------------------------------------------
   layout: hbs`
-    {{#unless hidden}}
-      {{! Page wide background that closes dropdown on click, not needed for keyboard users }}
-      <div class="rad-dropdown-background"
-        aria-hidden="false"
-        {{action "hide"}}>
-      </div>
-    {{/unless}}
-
     {{#if Target}}
       {{#rad-dropdown/target
         id=(concat 'aria-labelledby-' elementId)
@@ -216,19 +307,24 @@ export default Component.extend({
       {{/rad-dropdown/content}}
     {{/if}}
 
-    {{yield (hash
-      target=(component 'rad-dropdown/target'
-        id=(concat 'aria-labelledby-' elementId)
-        brand=brand
-        click=(action (if hidden 'show' 'hide'))
-        link=(not buttonStyle)
-        hidden=hidden)
-      content=(component 'rad-dropdown/content'
-        aria-labelledby=(concat 'aria-labelledby-' elementId)
-        dropdownMenu=dropdownMenu
-        hidden=hidden)
-      menu-item=(component 'rad-dropdown/menu-item'
-        hide=(action 'hide'))
-    ) (action 'show') (action 'hide') hidden}}
+    {{yield
+      (hash
+        target=(component 'rad-dropdown/target'
+          id=(concat 'aria-labelledby-' elementId)
+          brand=brand
+          click=(action (if hidden 'show' 'hide'))
+          link=(not buttonStyle)
+          hidden=hidden)
+        content=(component 'rad-dropdown/content'
+          aria-labelledby=(concat 'aria-labelledby-' elementId)
+          dropdownMenu=dropdownMenu
+          hidden=hidden)
+        menu-item=(component 'rad-dropdown/menu-item'
+          hide=(action 'hide'))
+      )
+      (action 'show')
+      (action 'hide')
+      hidden
+    }}
   `
 });
