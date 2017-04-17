f4125231a0ff610e2bf335d303c6ac2a8d68241a
Dan <hedgecock.d@gmail.com>
2017-04-17 09:52:22 -0700

    Do not import template compiler for test environments

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,5 +1,3 @@
-/*jshint node:true*/
-/* global require, module */
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
@@ -14,13 +12,19 @@ module.exports = function(defaults) {
     }
   });
 
-  // Import Prism deps
-  app.import('bower_components/prismjs/prism.js');
-  app.import('bower_components/prismjs/components/prism-handlebars.js');
-  app.import('bower_components/prismjs/components/prism-javascript.js');
+  // We use the test app as our demo app with production and for development
+  // with dev builds. In test, don't import external demo deps. (Trying to
+  // import template compiler in older Ember version test suites will blow
+  // up the tests)
+  if (app.env !== 'test') {
+    // Import template compiler for documentation
+    app.import('vendor/ember/ember-template-compiler.js');
 
-  // Import template compiler for documentation
-  app.import('vendor/ember/ember-template-compiler.js');
+    // Import Prism deps
+    app.import('bower_components/prismjs/prism.js');
+    app.import('bower_components/prismjs/components/prism-handlebars.js');
+    app.import('bower_components/prismjs/components/prism-javascript.js');
+  }
 
   return app.toTree();
 };
